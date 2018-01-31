cb0d041472cc47978f71e50879274f9cc04cbdca
Cristian Campos <cristian.campos@globant.com>
2018-01-31 16:26:14 -0300

    primer comit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { HttpModule } from '@angular/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/posts/posts.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts/';
+  constructor(private http: Http) {
+    http.get(this.url)
+      .subscribe( response => {
+        this.posts = response.json();
+      });
+  }
+
+  createPost ( input: HTMLInputElement){
+    const post: any = {title: input.value};
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe(response => {
+      post.id = response.json().id;
+      this.posts.splice(0, 0, post);
+      console.log(response.json());
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
